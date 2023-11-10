cd22ce9e568a4db3b25459afdf56c45e803d4c70
Antonio <taminto.a@gmail.com>
2023-11-10 17:55:16 +0100

    aggiunta la stampa dei nomi

--- Main2.java
+++ Main2.java
@@ -7,4 +7,10 @@ public class Main2 {
     String[2] = "Mattia Lattanzio";
     String[3] = "Samira Saud";
 
+
+
+    for(int i = 0; i < String.length; i++){
+        System.out.println(nomeCognomeStudenti[i]);
+    }
 }
+
