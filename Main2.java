d4bc6d42ca466c74ace268f3795999e03521415a
Elian <elianbertola14@gmail.com>
2023-11-10 17:57:12 +0100

    aggiunto il mio nome

--- Main2.java
+++ Main2.java
@@ -6,7 +6,7 @@ public class Main2 {
     String[1] = "Antonio Taminto";
     String[2] = "Mattia Lattanzio";
     String[3] = "Samira Saud";
-
+    String[4] = "Elian Bertola";
 
 
     for(int i = 0; i < String.length; i++){
