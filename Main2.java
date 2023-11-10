10e63a5f204753db6ee08d86eebe9fcd42ac05d7
ET <tacconellielisabetta50@gmail.com>
2023-11-10 18:24:23 +0100

    aggiunto il mio nome in posizione 0

--- Main2.java
+++ Main2.java
@@ -2,7 +2,7 @@ public class Main2 {
     public static void main(String[] args) {
         String[] nomeCognomeStudenti = new String[6];
     }
-
+    nomeCognomeStudenti[0] = "Elisabetta Tacconelli";
     String[1] = "Antonio Taminto";
     String[2] = "Mattia Lattanzio";
 
