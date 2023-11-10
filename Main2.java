e87e6a871b6adfc7d3809facc9ddb8decd3ce7d7
Antonio <taminto.a@gmail.com>
2023-11-10 17:43:07 +0100

    aggiunto nome nella posizione 1

--- Main2.java
+++ Main2.java
@@ -1,12 +1,7 @@
 public class Main2 {
-
-
     public static void main(String[] args) {
-
         String[] nomeCognomeStudenti = new String[6];
-
-
     }
-
+    String[1] = "Antonio Taminto";
 
 }
