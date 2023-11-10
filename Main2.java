458bb26f4faba07c8c5b08dcffb789adebfaabd3
Antonio <taminto.a@gmail.com>
2023-11-10 18:05:44 +0100

    risolto i bug che avevo inserito

--- Main2.java
+++ Main2.java
@@ -3,13 +3,13 @@ public class Main2 {
         String[] nomeCognomeStudenti = new String[6];
 
 
-        String[1] = "Antonio Taminto";
-        String[2] = "Mattia Lattanzio";
-        String[3] = "Samira Saud";
-        String[4] = "Elian Bertola";
+        nomeCognomeStudenti[1] = "Antonio Taminto";
+        nomeCognomeStudenti[2] = "Mattia Lattanzio";
+        nomeCognomeStudenti[3] = "Samira Saud";
+        nomeCognomeStudenti[4] = "Elian Bertola";
 
 
-        for (int i = 0; i < String.length; i++) {
+        for (int i = 0; i < nomeCognomeStudenti.length; i++) {
             System.out.println(nomeCognomeStudenti[i]);
         }
     }
