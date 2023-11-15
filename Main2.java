0d6fa9c02e82bf35dc1c854791585a4e79d8c657
Antonio <taminto.a@gmail.com>
2023-11-15 15:21:12 +0100

    Aggiunto la data di nascita di Samira Saud

--- Main2.java
+++ Main2.java
@@ -4,10 +4,10 @@ public class Main2 {
 
         nomeCognomeStudenti[0] = "Elisabetta Tacconelli";
         nomeCognomeStudenti[1] = "Antonio Taminto 04/06/1998";
-        nomeCognomeStudenti[2] = "Mattia Lattanzio 08/01/1996";
-        nomeCognomeStudenti[3] = "Samira Saud";
+        nomeCognomeStudenti[2] = "Mattia Lattanzio";
+        nomeCognomeStudenti[3] = "Samira Saud 20/07/1995";
         nomeCognomeStudenti[4] = "Elian Bertola";
-        nomeCognomeStudenti[5] = "Ana Balasa 28/09/1997";
+        nomeCognomeStudenti[5] = "Ana Balasa";
 
 
         for (int i = 0; i < nomeCognomeStudenti.length; i++) {
