c1c5a86b134b32c6b06029b4a81f053a48827c67
anamariaow <new_annemarie@yahoo.com>
2023-11-15 19:46:47 +0100

    Risolto bug creato

--- Main2.java
+++ Main2.java
@@ -3,11 +3,11 @@ public class Main2 {
         String[] nomeCognomeStudenti = new String[6];
 
         nomeCognomeStudenti[0] = "Elisabetta Tacconelli";
-        nomeCognomeStudenti[1] = "Antonio Taminto";
+        nomeCognomeStudenti[1] = "Antonio Taminto 04/06/1998";
         nomeCognomeStudenti[2] = "Mattia Lattanzio";
         nomeCognomeStudenti[3] = "Samira Saud";
         nomeCognomeStudenti[4] = "Elian Bertola";
-        nomeCognomeStudenti[5] = "Ana Balasa";
+        nomeCognomeStudenti[5] = "Ana Balasa 28/09/1997";
 
 
         for (int i = 0; i < nomeCognomeStudenti.length; i++) {
