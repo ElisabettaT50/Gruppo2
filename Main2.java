427b1e29d0cf86ffcb711491f6a3e24a43c1d5e4
Antonio <taminto.a@gmail.com>
2023-11-16 19:29:47 +0100

    eliminato il test

--- Main2.java
+++ Main2.java
@@ -1,6 +1,6 @@
 public class Main2 {
     public static void main(String[] args) {
-        String[] nomeCognomeStudenti = new String[7];
+        String[] nomeCognomeStudenti = new String[6];
 
         nomeCognomeStudenti[0] = "Elisabetta Tacconelli";
         nomeCognomeStudenti[1] = "Antonio Taminto 04/06/1998";
@@ -8,7 +8,6 @@ public class Main2 {
         nomeCognomeStudenti[3] = "Samira Saud";
         nomeCognomeStudenti[4] = "Elian Bertola";
         nomeCognomeStudenti[5] = "Ana Balasa 28/09/1997";
-        nomeCognomeStudenti[6] = "test";
 
         for (int i = 0; i < nomeCognomeStudenti.length; i++) {
             System.out.println(nomeCognomeStudenti[i]);
