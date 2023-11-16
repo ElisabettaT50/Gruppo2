49aada27f396c5419c3fca5bfb493a57c8eb4b07
Samira saud <samirasaudit@gmail.com>
2023-11-16 18:32:46 +0100

    rimossa per esercizio samira

--- Main2.java
+++ Main2.java
@@ -5,7 +5,6 @@ public class Main2 {
         nomeCognomeStudenti[0] = "Elisabetta Tacconelli 25/04/1993";
         nomeCognomeStudenti[1] = "Antonio Taminto 04/06/1998";
         nomeCognomeStudenti[2] = "Mattia Lattanzio 08/01/1996";
-        nomeCognomeStudenti[3] = "Samira Saud 20/07/1995";
         nomeCognomeStudenti[4] = "Elian Bertola";
         nomeCognomeStudenti[5] = "Ana Balasa 28/09/1997";
 
