6bb283769af6b4614af86ef86b585f5a606bdb71
Elian <elianbertola14@gmail.com>
2023-11-17 15:46:54 +0100

    ho messo la mia data e tolto un bug

--- Main2.java
+++ Main2.java
@@ -2,11 +2,11 @@ public class Main2 {
     public static void main(String[] args) {
         String[] nomeCognomeStudenti = new String[6];
 
-        nomeCognomeStudenti[0] = "Elisabetta Tacconelli 25/04/1993;
+        nomeCognomeStudenti[0] = "Elisabetta Tacconelli 25/04/1993";
         nomeCognomeStudenti[1] = "Antonio Taminto 04/06/1998";
         nomeCognomeStudenti[2] = "Mattia Lattanzio 08/01/1996";
         nomeCognomeStudenti[3] = "Samira Saud  20/07/1995";
-        nomeCognomeStudenti[4] = "Elian Bertola";
+        nomeCognomeStudenti[4] = "Elian Bertola 14/03/2000";
         nomeCognomeStudenti[5] = "Ana Balasa 28/09/1997";
 
         for (int i = 0; i < nomeCognomeStudenti.length; i++) {
